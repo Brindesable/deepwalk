e4c630aa525c781a948c6279b006212f8fc9540b
Brindesable <ollivier.kilian@gmail.com>
2017-01-05 01:01:17 +0000

    Email notification

--- build-docker.sh
+++ build-docker.sh
@@ -1,3 +1,1 @@
-git clone https://github.com/phanein/deepwalk.git deepwalk \
-&& cd deepwalk \
-&& docker build -t deepwalk .
+docker build -t deepwalk .
--- run-docker.sh
+++ run-docker.sh
@@ -1,5 +1,15 @@
+INPUT=karate.adjlist
+OUTPUT=karate.ebd
+
 docker run -v `pwd`/share:/usr/share deepwalk:latest \
 python /usr/deepwalk/deepwalk \
 --format adjlist \
---input /usr/share/karate.adjlist \
---output /usr/share/karate.ebd
+--input /usr/share/$INPUT \
+--output /usr/share/$OUTPUT
+
+curl -s --user 'api:key-xxx' \
+https://api.mailgun.net/v3/sandboxxxx.mailgun.org/messages \
+    -F from='Mailgun Sandbox <postmaster@sandboxxxx.mailgun.org>' \
+    -F to='xxx <xxx@xxx.com>' \
+    -F subject='Deepwalk' \
+    -F text="The computation of $OUTPUT just ended."
