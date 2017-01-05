03190790626ca4b4df606d6aa66fc3d2c64b6201
Brindesable <ollivier.kilian@gmail.com>
2017-01-05 02:00:09 +0000

    Add logs

--- run-docker.sh
+++ run-docker.sh
@@ -1,15 +1,24 @@
 INPUT=karate.adjlist
 OUTPUT=karate.ebd
 
+rm -rf logs &&
+mkdir -p logs &&
 docker run -v `pwd`/share:/usr/share deepwalk:latest \
 python /usr/deepwalk/deepwalk \
 --format adjlist \
 --input /usr/share/$INPUT \
---output /usr/share/$OUTPUT
-
+--output /usr/share/$OUTPUT > logs/stdout 2> logs/stderr && \
+DW_STDOUT=`cat logs/stdout` && \
+DW_STDERR=`cat logs/stderr` && \
 curl -s --user 'api:key-xxx' \
-https://api.mailgun.net/v3/sandboxxxx.mailgun.org/messages \
-    -F from='Mailgun Sandbox <postmaster@sandboxxxx.mailgun.org>' \
-    -F to='xxx <xxx@xxx.com>' \
+    https://api.mailgun.net/v3/xxx.mailgun.org/messages \
+    -F from='Mailgun Sandbox <postmaster@xxx.mailgun.org>' \
+    -F to='Kilian Ollivier <xxx@xxx.com>' \
     -F subject='Deepwalk' \
-    -F text="The computation of $OUTPUT just ended."
+    -F text="The computation of $OUTPUT just ended.
+
+stdout : 
+$DW_STDOUT
+
+stderr : 
+$DW_STDERR"
