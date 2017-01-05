050720308cfbd287230080271848f2e4d638e581
Brindesable <ollivier.kilian@gmail.com>
2017-01-05 02:17:33 +0000

    Add more parameters

--- run-docker.sh
+++ run-docker.sh
@@ -1,11 +1,20 @@
 INPUT=karate.adjlist
+FORMAT=adjlist
 OUTPUT=karate.ebd
+NB_WALKS=10
+REP_SIZE=64
+WINDOW_SIZE=5
+WORKERS=1
 
 rm -rf logs &&
 mkdir -p logs &&
 docker run -v `pwd`/share:/usr/share deepwalk:latest \
 python /usr/deepwalk/deepwalk \
---format adjlist \
+--format $FORMAT \
+--number-walks $NB_WALKS \
+--representation-size $REP_SIZE \
+--window-size $WINDOW_SIZE \
+--workers $WORKERS \
 --input /usr/share/$INPUT \
 --output /usr/share/$OUTPUT > logs/stdout 2> logs/stderr && \
 DW_STDOUT=`cat logs/stdout` && \
